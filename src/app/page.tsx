e827fa376cb16d55e88ab29284ca1e71341028c4
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2024-09-09 22:59:55 -0400

    :sparkles: feat: Front end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,57 @@
-import Image from "next/image";
+import type { User } from '@/@types'
+import { GET } from '@/actions/GET'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import dayjs from 'dayjs'
+import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+  const usuarios: User[] = await GET()
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main className="w-full flex justify-center items-center flex-wrap gap-3 p-3">
+      {usuarios.map(person => (
+        <Card
+          key={Number(person.id)}
+          className={`w-11/12 md:w-96 relative border-l-[8px] ${person.gender === 'male' ? 'border-l-blue-700' : 'border-l-pink-700'}`}
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+          <CardHeader className=" flex flex-row  justify-start ">
+            <CardTitle className="p-0">{`${person.name.first} ${person.name.last}`}</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-row ">
+            <div>
+              <Image
+                src={person.picture.large}
+                width={150}
+                height={150}
+                alt="imagem de perfil do usuario"
+                className={`absolute right-1 top-1  rounded-lg   w-[90px] h-[90px] 
+                   bject-center object-cover 
+                   ${person.gender === 'male' ? 'border-blue-700' : 'border-pink-700'} 
+                   border-[3px]
+                  `}
+              />
+            </div>
+            <div className="flex flex-col ml-2">
+              <CardDescription className="mb-1 text-md">
+                <span className="text-black">Genero: </span>
+                {person.gender}
+              </CardDescription>
+              <CardDescription className="mb-1 text-md">
+                <span className="text-black">Email: </span>
+                {person.email}
+              </CardDescription>
+              <CardDescription className="mb-1 text-md">
+                <span className="text-black">Idade: </span>
+                {person.dob.age} anos
+              </CardDescription>
+              <CardDescription className="mb-1 text-md">
+                <span className="text-black">Data de Nasimento: </span>
+                {dayjs(person.dob.date).format('DD/MM/YYYY')}
+              </CardDescription>
+            </div>
+          </CardContent>
+        </Card>
+      ))}
+    </main>
+  )
 }
